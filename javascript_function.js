aeb2bf76160e5f7e7a0ed16fa816387920cfcf78
lovelybia <mylovelybia@gmail.com>
2015-09-19 22:00:17 -0700

    javasctip function explanation

--- javascript_function.js
+++ javascript_function.js
@@ -1,6 +1,25 @@
-var get_factorial = function(m){
-	total = 1;
-	for(var index <=m; index++){
-		total = total * index;
-	} return total;
+
+var get_sum = function(m){
+	var total  = 0 ;
+	for(var index = 0  ; index <= m ;  index++ ){
+		total = total + index;
+	}
+	return total;
 }
+
+var get_factorial_and_sum = function(m){
+	var total = 1;
+	for(var index = 1 ; index <= m; index++){
+		total = total * index;
+	} 
+	return total + get_sum(m);
+}
+
+function sum_recursive(m){
+	if(m <= 1 )
+		return 1;
+	return m + sum_recursive(m-1);
+}
+
+
+
